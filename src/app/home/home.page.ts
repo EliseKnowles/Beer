9a6236f208640c93e58ca1d031c08f77f2d0af19
Marvin Mensah <marvin.mensah@edu.devinci.fr>
2020-05-27 11:14:06 +0200

    ok

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActionSheetController, Platform, AlertController } from '@ionic/angular';
+
+import {
+  GoogleMaps,
+  GoogleMap,
+  Environment
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  map: GoogleMap;
+    constructor(
+  
+        public alertController: AlertController,
+        public actionCtrl: ActionSheetController,
+        private platform: Platform
 
-  constructor() {}
-
+    ) {
+        if (this.platform.is('cordova')) {
+        
+    }
+  }
+  ngOnInit() {
+    this.loadMap();
+  }
+  
+  loadMap() {
+  
+    
+    Environment.setEnv({
+      API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyDvj-kehos36EKwDS2WM7twOkupMS0f6kY', // Clée Google map
+      API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyDvj-kehos36EKwDS2WM7twOkupMS0f6kY'
+    });
+    this.map = GoogleMaps.create('ƒ', {
+      camera: {
+        target: {
+          lat: 43.610769,
+          lng: 3.876716
+        },
+        zoom: 12,
+        tilt: 30
+      }
+    });
+}
 }
