d59552a1ae07628ee0a8a96f2a7d0aaaa093d37d
Marvin Mensah <marvin.mensah@edu.devinci.fr>
2020-05-31 00:42:07 +0200

    add page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './home-page/home-page.component';
 import { DiscoverComponent } from './discover/discover.component';
-<<<<<<< HEAD
 import { BarPagePage } from './bar-page/bar-page.page';
 
 
-=======
 import { ModifBarComponent } from './modif-bar/modif-bar.component';
->>>>>>> cc1729f97676064f4006e5b07895d16c79ac1311
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
   {path: 'discover', component: DiscoverComponent},
-<<<<<<< HEAD
   {path: 'bar-page', component: BarPagePage },
-=======
   {path: 'modif', component: ModifBarComponent},
->>>>>>> cc1729f97676064f4006e5b07895d16c79ac1311
 ];
 
 @NgModule({
