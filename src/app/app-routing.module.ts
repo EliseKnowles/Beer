25bcdbc91e1ff62676e0801bf7a142e75f1f443f
Florian <flo78480@hotmail.fr>
2020-05-31 14:39:41 +0200

    modif json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './home-page/home-page.component';
-import { DiscoverComponent } from './discover/discover.component';
 import { BarPagePage } from './bar-page/bar-page.page';
 
 
 import { ModifBarComponent } from './modif-bar/modif-bar.component';
-import { ResultatsComponent } from './resultats/resultats.component';
+import { DiscoverComponent } from './discover/discover.component';
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
   {path: 'discover', component: DiscoverComponent},
   {path: 'bar-page', component: BarPagePage },
-  {path: 'modif', component: ModifBarComponent},
-  {path: 'resultats', component: ResultatsComponent},
+  {path: 'modif', component: ModifBarComponent },
 ];
 
 @NgModule({
